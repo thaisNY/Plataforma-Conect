1a83313f775857f6efad52ad85a950f863ce4e71
Thais Rodrigues <thais.th90@gmail.com>
2026-02-17 23:38:04 -0300

    bug to fix

--- src/pages/paginainicial/Paginainicial.jsx
+++ src/pages/paginainicial/Paginainicial.jsx
@@ -1,8 +1,41 @@
+import s from './PaginaInicial.module.scss'
+import banner from '../../assets/imagens/banner.png'
+import arrow from "../../assets/icones/arrow-icon.png";
+
 const Paginainicial = () => {
   return (
-    <main>
-      <h1>Página Inicial</h1>
-    </main>
+
+  <main className={s.main}>
+   
+    <section className={s.info}>
+      <h1>Projetos Sociais que transformam</h1>
+      <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
+      <button>
+        Cadastrar Empresa <img src={arrow} alt="Icone de uma seta" />
+      </button>
+
+      <section className={s.infoDados}>
+       
+        <article>
+          <h2>500+</h2>
+          <h3>Empresas Voluntárias</h3>
+        </article>
+        <article>
+          <h2>1.2K+</h2>
+          <h3>Projetos Realizados</h3>
+        </article>
+            
+        <article>
+          <h2>50K+</h2>
+          <h3>Vidas Impactadas</h3>
+        </article>
+      </section>
+
+    </section> 
+    
+    <img src={banner} alt="Imagem da mao segurando o globo representando responsabilidade social e ambiental" />
+    
+  </main>
   )
 }
 
