dbe06403acf5fcf3fff6bbc709d83d4ba8193ef7
Thais Rodrigues <thais.th90@gmail.com>
2026-02-19 23:52:20 -0300

    Pag mentoria

--- src/pages/mentoria/Mentoria.jsx
+++ src/pages/mentoria/Mentoria.jsx
@@ -1,9 +1,44 @@
-
+import Inforcard from "../../components/infocard/Inforcard";
+import mentoria from "../../assets/imagens/mentoria.png";
+import compartilhar from "../../assets/imagens/compartilhar.png";
+import acompanhar from "../../assets/imagens/acompanhar.png";
+import s from "../Style.module.scss";
 
 const Mentoria = () => {
   return (
-    <main>
-        <h1>Pagina de Mentoria</h1>
+    <main className={s.main}>
+        <h1>Mentoria</h1>
+        <section>
+          <article>
+            <Inforcard
+              image= {mentoria}
+              alt= "Mãoes gesticulando sobre relatorios e gráficos"
+              subtitulo= "Mentoria de Carreira e Emprego"
+              paragrafo= "Orientação sobre currículo, entrevistas e primeiros passos no mercado de trabalho."
+              textoBotao= "Quero participar"
+            />
+          </article>
+
+          <article>
+            <Inforcard
+              image= {compartilhar}
+              alt= "Pessoas compartilhando conhecimento em uma reunião"
+              subtitulo= "Compartilhe Experiência"
+              paragrafo= "Oriente jovens e profissionais iniciantes em sua área."
+              textoBotao= "Quero participar"
+            />
+          </article>
+
+          <article>
+            <Inforcard
+              image= {acompanhar}
+              alt= "Mulher acompanhando o desenvolvimento de um jovem em um ambiente de trabalho"
+              subtitulo= "Acompanhamento"
+              paragrafo= "Participe como guia em jornadas de aprendizado e desenvolvimento."
+              textoBotao= "Quero participar"
+            />
+          </article>
+        </section>
     </main>
   )
 }
