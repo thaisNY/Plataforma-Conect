eca104e086ece4ad5a578f9740c8e128da8fa366
Thais Rodrigues <thais.th90@gmail.com>
2026-02-20 11:32:21 -0300

    Pag eventos

--- src/pages/usuario/Usuario.jsx
+++ src/pages/usuario/Usuario.jsx
@@ -1,9 +1,31 @@
-
+import s from "./usuario.module.scss";
+import { IoLocationOutline } from "react-icons/io5";
 
 const Usuario = () => {
   return (
-    <main>
-        <h1>Pagina de Usuario</h1>
+    <main className={s.main}>
+       <section>
+        <img src="https://avatars.githubusercontent.com/u/71556315?v=4" alt="Foto de perfil do usuário" />
+        <div className={s.div}>
+          <h1>Thais Rodrigues</h1>
+          <h2>Voluntária Ativa</h2>
+          <p>Apaixonado por fazer a diferença na comunidade. Acredito que pequenas ações podem transformar vidas e estou sempre em busca de novas oportunidades para ajudar.</p>
+      
+          <ul>
+            <li> <IoLocationOutline />Caruaru - PE</li>
+            <li>thais@email.com</li>
+            <li>Membro desde Janeiro 2022</li>
+          </ul>
+
+          <ul>
+            <li>Educação</li>
+            <li>Meio Ambiente</li>
+            <li>Assistência Social</li>
+            <li>Design</li>
+          </ul>
+
+        </div>
+       </section>
     </main>
   )
 }
